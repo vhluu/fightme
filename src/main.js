927479e05e69cc451787afb0a838fcef35208e74
Vanna <vhluu@ucsd.edu>
2019-03-12 21:41:12 -0700

    adding vue-socket.io

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import { store } from './store';
+
+import VueSocketio from 'vue-socket.io';
+Vue.use(VueSocketio, 'http://localhost:5000/');
 
 export const eventBus = new Vue();
 
 new Vue({
   el: '#app',
+  store,
   render: h => h(App)
-})
+});
+

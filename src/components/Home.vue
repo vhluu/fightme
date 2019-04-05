bffd7466c26a1e72eaf68bd9c40da90d8584d454
Vanna <vhluu@ucsd.edu>
2019-04-05 10:50:19 -0700

    adding new game and join game functionality to homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,41 @@
 <template>
   <div>
     <h1>Can't settle on who's gunna pick the movie? Or who's gunna drive?<br>Fight each other virtually and see who wins!</h1>
-    <button @click="newGame">New Game</button>
-    <div>
+    <div class="btn"><a href="/add-game">New Game</a></div>
+    <!-- <div> -->
       <span>OR</span>
-      <div>
-        <input type="text" placeholder="Invite Code">
-        <button @click="joinGame">Join Game</button>
-      </div> 
-    </div>
+      <!-- <div>
+        <input type="text" placeholder="Invite Code"> -->
+        <div class="btn"><a href="/join-game">Join Game</a></div>
+      <!-- </div>  -->
+    <!-- </div> -->
   </div>
 </template>
 
 <script>
 import { eventBus } from '../main';
 export default {
-  methods: {
-    newGame() {
-      eventBus.$emit('changeScreen', 'app-character-select');
-    },
-    joinGame() {
-      eventBus.$emit('changeScreen', 'app-character-select');
-    }
-  }
+  // methods: {
+  //   async newGame() {
+  //     // generate invite code
+  //     var inviteCode = "";
+  //     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  //     for (var i = 0; i < 5; i++) {
+  //       inviteCode += possible.charAt(Math.floor(Math.random() * possible.length));
+  //     }
+
+  //     let game = {
+  //         invite_code : inviteCode,
+  //       }
+      
+  //     axios.post('http://localhost:5000/api/game', game);
+
+  //     eventBus.$emit('changeScreen', 'app-character-select');
+  //   },
+  //   joinGame() {
+  //     eventBus.$emit('changeScreen', 'app-character-select');
+  //   }
+  // }
 }
 </script>

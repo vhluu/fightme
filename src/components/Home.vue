0b6556ed53d519a28c150f6d88ee19c02830efba
Vanna <vhluu@ucsd.edu>
2019-04-15 00:31:18 -0700

    updating homepage text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-    <h1>Can't settle on who's gunna pick the movie? Or who's gunna drive?<br>Fight each other virtually and see who wins!</h1>
+    <h1>Fight a friend over any decision!</h1>
+    <p>Too polite and fighting over who's gunna pay the bill?<br>Or are you guys too lazy and fighting over who's gunna drive?</p>
+    <p><strong>Whatever it is, tell them "Fight Me!" and see who wins ;)</strong></p>
     <a class="btn" href="/#/add-game">New Game</a>
     <a class="btn btn-light" href="/#/join-game">Join Game</a>
   </div>
@@ -15,5 +17,9 @@ export default {
 <style>
 .home {
   margin: 0 auto;
+  background: #fff;
+  height: 100%;
+  width: 100%;
+  padding-top: 20vh;
 }
 </style>

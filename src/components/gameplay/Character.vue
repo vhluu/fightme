fa2f2e24c930e48f3d7ac19a857fd61adfa14e66
Vanna <vhluu@ucsd.edu>
2019-04-17 12:18:17 -0700

    adding bg to game and fixing styles

--- src/components/gameplay/Character.vue
+++ src/components/gameplay/Character.vue
@@ -1,21 +1,17 @@
 <template>
-  <div>
+  <div class="character">
     <h3>{{nickname}}</h3>
     <div v-if="type == 'fire'">
       <img src="../../assets/fire_dino.png" alt="Fire Dino">
-      <h4>Fire 🔥</h4>
     </div>
     <div v-else-if="type == 'water'">
       <img src="../../assets/water_dino.png" alt="Water Dino">
-      <h4>Water 🌊</h4>
     </div>
     <div v-else-if="type == 'earth'">
       <img src="../../assets/earth_dino.png" alt="Earth Dino">
-      <h4>Earth ⛰️</h4>
     </div>
     <div v-else-if="type == 'air'">
       <img src="../../assets/air_dino.png" alt="Air Dino">
-      <h4>Air 💨</h4>
     </div>
   </div>
 </template>
@@ -25,4 +21,5 @@ export default {
   props: ['type', 'nickname']
 }
 </script>
+
 

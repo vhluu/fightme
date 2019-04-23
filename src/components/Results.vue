0766d86368a046de74bbda2893f06ef283d8a1f8
Vanna <vhluu@ucsd.edu>
2019-04-22 17:07:29 -0700

    adding winner results style with crown

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="results">
-    <div v-if="$myGlobalVars.won">You won!!</div>
-    <div v-else>You lost :(</div>
-    <button @click="goHome">Finish Game</button>
+    <div v-if="$myGlobalVars.won">
+      <h1>You won!!</h1>
+      <div class="character-result">
+        <img class="crown" :class="$myGlobalVars.chosenType" src="../assets/crown.png" alt="">
+        <app-character class="results" :type="$myGlobalVars.chosenType"></app-character>
+      </div>
+    </div>
+    <div v-else>
+      <h1>You lost :(</h1>
+      <div class="character-result">
+        <img class="crown" :class="$myGlobalVars.chosenType" src="../assets/crown.png" alt="">
+        <app-character class="results" :type="$myGlobalVars.chosenType"></app-character>
+      </div>
+    </div>
+    <div class="btn" @click="goHome">Finish Game</div>
   </div>
 </template>
 
 <script>
+import Character from './gameplay/Character.vue';
+
 export default {
+  components: {
+    'app-character': Character
+  },
   methods: {
     goHome() {
       this.$router.push({
@@ -17,3 +34,40 @@ export default {
   }
 }
 </script>
+
+<style>
+.character-result {
+  position: relative;
+}
+
+.results.character img {
+  height: 47vw;
+}
+
+.crown {
+  position: absolute;
+}
+.crown.water {
+  width: 10%;
+  left: 33%;
+  top: -2%;
+  transform: rotate(18deg);
+}
+.crown.fire {
+  width: 11%;
+  left: 42%;
+  transform: rotate(11deg);
+  top: 1%;
+}
+.crown.earth {
+  width: 9%;
+  left: 22%;
+  top: 52.5%;
+}
+.crown.air {
+  left: 43%;
+  transform: rotate(32deg);
+  width: 9%;
+  top: 4%;
+}
+</style>

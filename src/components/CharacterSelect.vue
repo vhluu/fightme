3fb1c5d7f5121e2a561f07f9be136750be42f505
Vanna <vhluu@ucsd.edu>
2021-01-20 18:02:47 -0800

    updated dependencies and moved to firebase hosting

--- src/components/CharacterSelect.vue
+++ src/components/CharacterSelect.vue
@@ -3,22 +3,22 @@
     <h1>Choose Your Fighter</h1>
     <div class="flex space-between mobile-hide">
       <div class="card card-hover character tablet-col-2 bg-fire" @click="chosenType = 'fire'; startGame()">
-        <img src="../assets/fire_dino.png">
+        <img src="/assets/fire_dino.png">
         <h3>Fire 🔥</h3>
         <svg v-if="chosenType == 'fire'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 191.667 191.667"><path d="M95.833 0C42.991 0 0 42.99 0 95.833s42.991 95.834 95.833 95.834 95.833-42.991 95.833-95.834S148.676 0 95.833 0zm55.029 79.646l-60.207 60.207a13.463 13.463 0 0 1-9.583 3.969c-3.62 0-7.023-1.409-9.583-3.969l-30.685-30.685a13.464 13.464 0 0 1-3.97-9.583c0-3.621 1.41-7.024 3.97-9.584a13.46 13.46 0 0 1 9.583-3.97c3.62 0 7.024 1.41 9.583 3.971l21.101 21.1 50.623-50.623a13.463 13.463 0 0 1 9.583-3.969c3.62 0 7.023 1.409 9.583 3.969 5.286 5.286 5.286 13.883.002 19.167z"></path></svg>
       </div>
       <div class="card card-hover character tablet-col-2 bg-water" @click="chosenType = 'water'; startGame()">
-        <img src="../assets/water_dino.png">
+        <img src="/assets/water_dino.png">
         <h3>Water 🌊</h3>
         <svg v-if="chosenType == 'water'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 191.667 191.667"><path d="M95.833 0C42.991 0 0 42.99 0 95.833s42.991 95.834 95.833 95.834 95.833-42.991 95.833-95.834S148.676 0 95.833 0zm55.029 79.646l-60.207 60.207a13.463 13.463 0 0 1-9.583 3.969c-3.62 0-7.023-1.409-9.583-3.969l-30.685-30.685a13.464 13.464 0 0 1-3.97-9.583c0-3.621 1.41-7.024 3.97-9.584a13.46 13.46 0 0 1 9.583-3.97c3.62 0 7.024 1.41 9.583 3.971l21.101 21.1 50.623-50.623a13.463 13.463 0 0 1 9.583-3.969c3.62 0 7.023 1.409 9.583 3.969 5.286 5.286 5.286 13.883.002 19.167z"></path></svg>
       </div>
       <div class="card card-hover character tablet-col-2 bg-earth" @click="chosenType = 'earth'; startGame()">
-        <img src="../assets/earth_dino.png">
+        <img src="/assets/earth_dino.png">
         <h3>Earth ⛰️</h3>
         <svg v-if="chosenType == 'earth'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 191.667 191.667"><path d="M95.833 0C42.991 0 0 42.99 0 95.833s42.991 95.834 95.833 95.834 95.833-42.991 95.833-95.834S148.676 0 95.833 0zm55.029 79.646l-60.207 60.207a13.463 13.463 0 0 1-9.583 3.969c-3.62 0-7.023-1.409-9.583-3.969l-30.685-30.685a13.464 13.464 0 0 1-3.97-9.583c0-3.621 1.41-7.024 3.97-9.584a13.46 13.46 0 0 1 9.583-3.97c3.62 0 7.024 1.41 9.583 3.971l21.101 21.1 50.623-50.623a13.463 13.463 0 0 1 9.583-3.969c3.62 0 7.023 1.409 9.583 3.969 5.286 5.286 5.286 13.883.002 19.167z"></path></svg>
       </div>
       <div class="card card-hover character tablet-col-2 bg-air" @click="chosenType = 'air'; startGame()">
-        <img src="../assets/air_dino.png">
+        <img src="/assets/air_dino.png">
         <h3>Air 💨</h3>
         <svg v-if="chosenType == 'air'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 191.667 191.667"><path d="M95.833 0C42.991 0 0 42.99 0 95.833s42.991 95.834 95.833 95.834 95.833-42.991 95.833-95.834S148.676 0 95.833 0zm55.029 79.646l-60.207 60.207a13.463 13.463 0 0 1-9.583 3.969c-3.62 0-7.023-1.409-9.583-3.969l-30.685-30.685a13.464 13.464 0 0 1-3.97-9.583c0-3.621 1.41-7.024 3.97-9.584a13.46 13.46 0 0 1 9.583-3.97c3.62 0 7.024 1.41 9.583 3.971l21.101 21.1 50.623-50.623a13.463 13.463 0 0 1 9.583-3.969c3.62 0 7.023 1.409 9.583 3.969 5.286 5.286 5.286 13.883.002 19.167z"></path></svg>
       </div>
@@ -28,28 +28,28 @@
         <ul class="glide__slides">
           <li class="glide__slide">
             <div class="card card-hover character tablet-col-2 bg-fire" @click="chosenType = 'fire'; startGame()">
-              <img src="../assets/fire_dino.png">
+              <img src="/assets/fire_dino.png">
               <h3>Fire 🔥</h3>
               <svg v-if="chosenType == 'fire'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 191.667 191.667"><path d="M95.833 0C42.991 0 0 42.99 0 95.833s42.991 95.834 95.833 95.834 95.833-42.991 95.833-95.834S148.676 0 95.833 0zm55.029 79.646l-60.207 60.207a13.463 13.463 0 0 1-9.583 3.969c-3.62 0-7.023-1.409-9.583-3.969l-30.685-30.685a13.464 13.464 0 0 1-3.97-9.583c0-3.621 1.41-7.024 3.97-9.584a13.46 13.46 0 0 1 9.583-3.97c3.62 0 7.024 1.41 9.583 3.971l21.101 21.1 50.623-50.623a13.463 13.463 0 0 1 9.583-3.969c3.62 0 7.023 1.409 9.583 3.969 5.286 5.286 5.286 13.883.002 19.167z"></path></svg>
             </div>
           </li>
           <li class="glide__slide">
             <div class="card card-hover character tablet-col-2 bg-water" @click="chosenType = 'water'; startGame()">
-              <img src="../assets/water_dino.png">
+              <img src="/assets/water_dino.png">
               <h3>Water 🌊</h3>
               <svg v-if="chosenType == 'water'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 191.667 191.667"><path d="M95.833 0C42.991 0 0 42.99 0 95.833s42.991 95.834 95.833 95.834 95.833-42.991 95.833-95.834S148.676 0 95.833 0zm55.029 79.646l-60.207 60.207a13.463 13.463 0 0 1-9.583 3.969c-3.62 0-7.023-1.409-9.583-3.969l-30.685-30.685a13.464 13.464 0 0 1-3.97-9.583c0-3.621 1.41-7.024 3.97-9.584a13.46 13.46 0 0 1 9.583-3.97c3.62 0 7.024 1.41 9.583 3.971l21.101 21.1 50.623-50.623a13.463 13.463 0 0 1 9.583-3.969c3.62 0 7.023 1.409 9.583 3.969 5.286 5.286 5.286 13.883.002 19.167z"></path></svg>
             </div>
           </li>
           <li class="glide__slide">
             <div class="card card-hover character tablet-col-2 bg-earth" @click="chosenType = 'earth'; startGame()">
-              <img src="../assets/earth_dino.png">
+              <img src="/assets/earth_dino.png">
               <h3>Earth ⛰️</h3>
               <svg v-if="chosenType == 'earth'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 191.667 191.667"><path d="M95.833 0C42.991 0 0 42.99 0 95.833s42.991 95.834 95.833 95.834 95.833-42.991 95.833-95.834S148.676 0 95.833 0zm55.029 79.646l-60.207 60.207a13.463 13.463 0 0 1-9.583 3.969c-3.62 0-7.023-1.409-9.583-3.969l-30.685-30.685a13.464 13.464 0 0 1-3.97-9.583c0-3.621 1.41-7.024 3.97-9.584a13.46 13.46 0 0 1 9.583-3.97c3.62 0 7.024 1.41 9.583 3.971l21.101 21.1 50.623-50.623a13.463 13.463 0 0 1 9.583-3.969c3.62 0 7.023 1.409 9.583 3.969 5.286 5.286 5.286 13.883.002 19.167z"></path></svg>
             </div>
           </li>
           <li class="glide__slide">
             <div class="card card-hover character tablet-col-2 bg-air" @click="chosenType = 'air'; startGame()">
-              <img src="../assets/air_dino.png">
+              <img src="/assets/air_dino.png">
               <h3>Air 💨</h3>
               <svg v-if="chosenType == 'air'" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 191.667 191.667"><path d="M95.833 0C42.991 0 0 42.99 0 95.833s42.991 95.834 95.833 95.834 95.833-42.991 95.833-95.834S148.676 0 95.833 0zm55.029 79.646l-60.207 60.207a13.463 13.463 0 0 1-9.583 3.969c-3.62 0-7.023-1.409-9.583-3.969l-30.685-30.685a13.464 13.464 0 0 1-3.97-9.583c0-3.621 1.41-7.024 3.97-9.584a13.46 13.46 0 0 1 9.583-3.97c3.62 0 7.024 1.41 9.583 3.971l21.101 21.1 50.623-50.623a13.463 13.463 0 0 1 9.583-3.969c3.62 0 7.023 1.409 9.583 3.969 5.286 5.286 5.286 13.883.002 19.167z"></path></svg>
             </div>
@@ -75,8 +75,10 @@
 <script>
 import { eventBus } from '../main';
 import * as io from 'socket.io-client';
-// import Glide from '@glidejs/glide'
-import Glide, { Images, Swipe, Controls } from '@glidejs/glide/dist/glide.modular.esm'
+
+import Glide, { Images, Swipe, Controls } from '@glidejs/glide/dist/glide.modular.esm';
+import '@glidejs/glide/dist/css/glide.core.min.css';
+import '@glidejs/glide/dist/css/glide.theme.min.css';
 
 export default {
   data: function() {
@@ -156,7 +158,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped>  
   .character {
     cursor: pointer;
     position: relative;
